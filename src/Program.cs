30156f5ee21ddd723b8b469ce0cf040c50acf9c5
Sergey Luganskiy <sergeylug2000@mail.ru>
2020-04-25 09:51:11 +0300

    Создание класса Indications

--- src/Program.cs
+++ src/Program.cs
@@ -10,6 +10,27 @@ namespace src
     {
         static void Main(string[] args)
         {
+
+
+        }
+    }
+
+    class Indications
+    {
+        public Indications()
+        {
+            temperature = 0;
+            wet = 0;
+            pressure = 0;
+
+        }
+        public double temperature;
+        public int wet;
+        public int pressure;
+
+        public string DateReturn()
+        {
+            return  "Темература:" + temperature.ToString() + "\n Влажность:" + wet.ToString() + "\n Давление:" + pressure.ToString();
         }
     }
 }
